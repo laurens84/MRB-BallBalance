196fc42564b82cd595f6d162e13085c59892b732
Nick <nickswaerdens@gmail.com>
2018-05-18 14:42:12 +0200

    Removed unnecessary files.

--- test/test_main.cpp
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-
-TEST_CASE( "Example Test Case" ) {
-    REQUIRE(10 == 10);
-}
